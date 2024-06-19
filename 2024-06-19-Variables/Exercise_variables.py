c59f25fcb75af51dad4e65c7183862a53e56a0d6
Yomathnotcool <dengzymathnt@outlook.com>
2024-06-19 13:44:10 +0200

    exercise about variables

---
+++ 2024-06-19-Variables/Exercise_variables.py
@@ -0,0 +1,11 @@
+var_1 = input("Please enter the first variables: ")
+var_2 = input("Please enter the second variables: ")
+
+print("The first variable is: ", var_1)
+print("The second variable is: ", var_2)
+
+var_null = var_2
+var_2 = var_1
+var_1 = var_null
+
+print(var_1, var_2)
