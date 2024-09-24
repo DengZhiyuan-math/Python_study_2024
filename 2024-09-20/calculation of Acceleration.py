13b2d8709343519341d286a8d5ef5ed967a17949
Yomathnotcool <dengzymathnt@gmail.com>
2024-09-24 22:48:54 +0200

    Update calculation of Acceleration.py

--- 2024-09-20/calculation of Acceleration.py
+++ 2024-09-20/calculation of Acceleration.py
@@ -1,12 +1,20 @@
 def get_input():
-    data = input("please enter distance, time and initial velocity, seperate the input with space")
-        distance = int(data[0])
-        time = int(data[1])
-        initial_velocity = int(data[2])
+    data = input("please enter distance, time and initial velocity, seperate the input with space:")
+    distance, time, initial_velocity = map(float, data.split())
     return distance, time, initial_velocity
 
-def calculate_acceleration():
+def calculate_acceleration(distance, time, initial_velocity):
+    if time == 0:
+        raise ValueError("time cannot be zero")
     acceleration = (distance - time * initial_velocity)
+    return acceleration
 
 
 
+if __name__ == "__main__":
+    try:
+        distance, time, initial_velocity = get_input()
+        acceleration = calculate_acceleration(distance, time, initial_velocity)
+        print(f"The acceleration is: {acceleration} m/s^2")
+    except ValueError as e:
+        print(e)
