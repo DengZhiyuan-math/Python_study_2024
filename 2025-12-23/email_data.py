8b80bc1ddc487d7ac28c6ddfbf5523f888d319eb
Yomathnotcool <dengzymathnt@gmail.com>
2025-12-23 22:49:03 +0100

    Update Python SDK to 3.11 and improve email_data.py
    Project configuration files updated to use Python 3.11 SDK. Refactored email_data.py to calculate and print the average spam confidence instead of printing each line.

--- 2025-12-23/email_data.py
+++ 2025-12-23/email_data.py
@@ -1,8 +1,13 @@
 # Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
+file_name = input("Enter file name: ")
+fh = open(file_name)
+summation = 0
+number_of_lines = 0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    print(line)
-print("Done")
+    position_space = line.rfind(" ")
+    number = float(line[position_space + 1:])
+    number_of_lines = number_of_lines + 1
+    summation = summation + number
+print("Average spam confidence:", summation / number_of_lines)
